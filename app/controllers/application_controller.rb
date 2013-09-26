d06a5bcdb7dcab321b4467808bb9aff2fca19698
paxell <e.paxman.design@gmail.com>
2013-09-26 21:07:15 +1000

    * Prevent error appearing when user not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
   end
 
   def current_user_id
